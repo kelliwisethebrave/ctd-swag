0b3fa398c1c407e5d1d991061519f956b8aecaef
kelliwisethebrave <198285550+kelliwisethebrave@users.noreply.github.com>
2025-12-02 20:14:54 -0700

    Add and update files up to Week 4

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import './index.css'
+import './assets/fonts/WorkSans/WorkSans-Regular.ttf'
+import './assets/fonts/WorkSans/WorkSans-ExtraBold.ttf'
+import './assets/fonts/WorkSans/WorkSans-Light.ttf'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
